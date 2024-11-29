a26be10b5606a31e1b7848aca12cc11cdc5cd89e
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-11-29 17:44:26 -0500

    Added price to seats and set their price

--- src/cinema/Seat.java
+++ src/cinema/Seat.java
@@ -3,16 +3,27 @@ package cinema;
 public class Seat {
     private int row;
     private int column;
+    private int price;
 
     public Seat(int row, int column) {
         this.row = row;
         this.column = column;
+        if (row <= 4) {
+            price = 10;
+        } else {
+            price = 8;
+        }
     }
 
+    //Getters
     public int getRow() {
         return row;
     }
     public int getColumn() {
         return column;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
