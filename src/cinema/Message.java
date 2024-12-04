5a8a63b1fd2aaa76a896f158dff17dc4d344e6b2
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-12-04 12:43:24 -0500

    Message object for messages to user

---
+++ src/cinema/Message.java
@@ -0,0 +1,13 @@
+package cinema;
+
+public class Message {
+    public final String error;
+
+    public Message(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
